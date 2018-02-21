fce60f109a9531b9ce2a55930bcc0d7c766749d7
shuchao <645904877@qq.com>
2018-02-21 22:43:24 +0800

    ...

--- src/module/main/index.js
+++ src/module/main/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {TabNavigator} from 'react-navigation';
+import {TabNavigator, TabBarBottom} from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Home from 'module/main/home';
 import Mark from 'module/main/mark';
@@ -10,35 +11,59 @@ export default TabNavigator({
         screen           : Home,
         navigationOptions: {
             tabBarLabel: '首页',
+            tabBarIcon : ({tintColor}) => <Icon name="ios-disc" size={18} color={tintColor}/>,
         }
     },
     "main/mark": {
         screen           : Mark,
         navigationOptions: {
-            tabBarLabel: '记忆',
+            tabBarLabel: '快照',
+            tabBarIcon : ({tintColor}) => <Icon name="md-expand" size={18} color={tintColor}/>,
+        }
+    },
+    "main/find": {
+        screen           : Mark,
+        navigationOptions: {
+            tabBarLabel: '发现',
+            tabBarIcon : ({tintColor}) => <Icon name="md-wifi" size={18} color={tintColor}/>,
+        }
+    },
+    "main/message": {
+        screen           : Mark,
+        navigationOptions: {
+            tabBarLabel: '消息',
+            tabBarIcon : ({tintColor}) => <Icon name="md-notifications" size={18} color={tintColor}/>,
         }
     },
     "main/mine": {
-        screen: Mine, navigationOptions: {
+        screen           : Mine,
+        navigationOptions: {
             tabBarLabel: '我的',
+            tabBarIcon : ({tintColor}) => <Icon name="ios-contact" size={18} color={tintColor}/>,
         }
     },
 }, {
-    tabBarPosition  : 'bottom',
-    swipeEnabled    : false,
-    animationEnabled: false,
-    tabBarOptions   : {
-        labelStyle             : {
-            fontSize  : 14,
-            lineHeight: 49,
+    navigationOptions: ({navigation}) => ({
+        tabBarIcon: ({focused, tintColor}) => {
+            const {routeName} = navigation.state;
+            let iconName;
+            if (routeName === 'Home') {
+                iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+            } else if (routeName === 'Settings') {
+                iconName = `ios-options${focused ? '' : '-outline'}`;
+            }
+
+            // You can return any component that you like here! We usually use an
+            // icon component from react-native-vector-icons
+            return <Icon name={iconName} size={25} color={tintColor}/>;
         },
-        style                  : {
-            height: 49
-        },
-        //     activeBackgroundColor  : 'white',
-        //     activeTintColor        : '#4ECBFC',
-        inactiveBackgroundColor: 'white',
-        inactiveTintColor      : '#aaa',
-        //     // showLabel              : false,
-    }
+    }),
+    tabBarComponent  : TabBarBottom,
+    tabBarPosition   : 'bottom',
+    animationEnabled : false,
+    swipeEnabled     : false,
+    tabBarOptions    : {
+        activeTintColor  : 'tomato',
+        inactiveTintColor: 'gray',
+    },
 });
