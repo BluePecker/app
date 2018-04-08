e736fbb10d78b54139ca21c5431a8105e360913a
shuchao <645904877@qq.com>
2018-04-08 11:07:22 +0800

    ...

--- src/module/main/home/album/index.js
+++ src/module/main/home/album/index.js
@@ -9,43 +9,11 @@ import {
 } from '@shoutem/ui';
 import Swiper from 'react-native-swiper';
 import {Modal, Animated, ScrollView, TouchableHighlight, TouchableWithoutFeedback} from 'react-native';
-import Icon from 'react-native-vector-icons/EvilIcons';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
 
 import Inject from 'module';
 import Css from './css';
 import Model from 'model/main/home';
-
-// class FadeInView extends Component {
-//     state = {
-//         fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
-//     }
-//
-//     componentDidMount() {
-//         Animated.timing(                  // Animate over time
-//             this.state.fadeAnim,            // The animated value to drive
-//             {
-//                 toValue : 1,                   // Animate to opacity: 1 (opaque)
-//                 duration: 1000,              // Make it take a while
-//             }
-//         ).start();                        // Starts the animation
-//     }
-//
-//     render() {
-//         let {fadeAnim} = this.state;
-//
-//         return (
-//             <Animated.View                 // Special animatable View
-//                 style={{
-//                     ...this.props.style,
-//                     opacity: fadeAnim,         // Bind opacity to animated value
-//                 }}
-//             >
-//                 {this.props.children}
-//             </Animated.View>
-//         );
-//     }
-// }
-
 
 class Album extends Component {
 
@@ -57,9 +25,13 @@ class Album extends Component {
     }
 
     componentDidMount() {
-        Animated.parallel([
-            Animated.spring(this.state.animated, {toValue: 1}),
-        ]).start();
+        Animated.spring(this.state.animated, {toValue: 1}).start();
+    }
+
+    convertPosition(n, i, x, y) {
+        y += (n / 3 - i / 3) * 100;
+        x += (n % 3 - i % 3) * 100;
+        return {x, y};
     }
 
     render() {
@@ -85,7 +57,7 @@ class Album extends Component {
                                             duration: 800,
                                         }).start();
                                     }} style={{margin: 0, padding: 0}}>
-                                        <Icon name="close" style={Css._close} size={20}/>
+                                        <EvilIcon name="close" style={Css._close} size={20}/>
                                     </Button>
                                 </View>
                                 <View style={Css._middle}>
@@ -95,7 +67,7 @@ class Album extends Component {
                                     <Button styleName="clear" onPress={() => {
                                         share && share();
                                     }} style={{margin: 0, padding: 0}}>
-                                        <Icon name="share-apple" style={Css._share} size={20}/>
+                                        <EvilIcon name="share-apple" style={Css._share} size={20}/>
                                     </Button>
                                 </View>
                             </View>
@@ -104,151 +76,78 @@ class Album extends Component {
                     bounces
                     automaticallyAdjustContentInsets
                 >
-                    {
-                        source.map((item, num) => {
-                            const scaleY = w / width * (item.height / item.width);
-                            return (
-                                <TouchableHighlight
-                                    onPress={() => {
-                                        this.props.navigation.goBack();
-                                        Animated.spring(this.state.animated, {
-                                            toValue : 0,
-                                            duration: 800,
-                                        }).start();
+                    {source.map((item, num) => {
+                        const scaleY = w / width * (item.height / item.width);
+                        return (
+                            <TouchableHighlight
+                                onPress={() => {
+                                    this.props.navigation.goBack();
+                                    Animated.spring(this.state.animated, {
+                                        toValue : 0,
+                                        duration: 800,
+                                    }).start();
+                                }}
+                                activeOpacity={1}
+                                key={num}
+                            >
+                                <ScrollView
+                                    showsVerticalScrollIndicator={false}
+                                    contentContainerStyle={{
+                                        height         : scaleY * width > h ? scaleY * width : h,
+                                        width          : w,
+                                        backgroundColor: '#000',
                                     }}
-                                    activeOpacity={1}
-                                    key={num}
                                 >
-                                    <ScrollView
-                                        showsVerticalScrollIndicator={false}
-                                        contentContainerStyle={{
-                                            height         : scaleY * width > h ? scaleY * width : h,
-                                            width          : w,
-                                            backgroundColor: '#000',
-                                        }}
-                                    >
-                                        <TouchableWithoutFeedback>
-                                            <Animated.Image
-                                                source={{
-                                                    resizeMode: 'contain',
-                                                    uri       : item.uri,
-                                                    cache     : 'force-cache',
-                                                }}
-                                                style={{
-                                                    width    : width,
-                                                    height   : height,
-                                                    top      : y,
-                                                    left     : x,
-                                                    position : 'absolute',
-                                                    opacity  : this.state.animated.interpolate({
-                                                        inputRange : [0, 1],
-                                                        outputRange: [0.5, 1],
-                                                    }),
-                                                    transform: [
-                                                        {
-                                                            translateX: this.state.animated.interpolate({
-                                                                inputRange : [0, 1],
-                                                                outputRange: [0, w / 2 - (x + width / 2)],
-                                                            }),
-                                                        },
-                                                        {
-                                                            translateY: this.state.animated.interpolate({
-                                                                inputRange : [0, 1],
-                                                                outputRange: [0, scaleY * width > h ? (scaleY * width) / 2 - (y + height / 2) : h / 2 - (y + height / 2)],
-                                                            }),
-                                                        },
-                                                        {
-                                                            scaleX: this.state.animated.interpolate({
-                                                                inputRange : [0, 1],
-                                                                outputRange: [1, w / width],
-                                                            }),
-                                                        },
-                                                        {
-                                                            scaleY: this.state.animated.interpolate({
-                                                                inputRange : [0, 1],
-                                                                outputRange: [1, scaleY],
-                                                            }),
-                                                        },
-                                                    ],
-                                                }}
-                                            />
-                                        </TouchableWithoutFeedback>
-                                    </ScrollView>
-                                </TouchableHighlight>
-                            );
-                        })
-                    }
-
-
-                    {/*<TouchableHighlight*/}
-                    {/*onPress={() => {*/}
-                    {/*this.props.navigation.goBack();*/}
-                    {/*}}*/}
-                    {/*activeOpacity={1}*/}
-                    {/*>*/}
-                    {/*<ScrollView*/}
-                    {/*showsVerticalScrollIndicator={false}*/}
-                    {/*contentContainerStyle={{*/}
-                    {/*height         : w / width * (source[index].height / source[index].width) * width > h ? w / width * (source[index].height / source[index].width) * width : h,*/}
-                    {/*width          : w,*/}
-                    {/*backgroundColor: 'red',*/}
-                    {/*}}*/}
-                    {/*>*/}
-                    {/*<TouchableWithoutFeedback>*/}
-                    {/*<Animated.Image*/}
-                    {/*style={{*/}
-                    {/*width    : width,*/}
-                    {/*height   : height,*/}
-                    {/*top      : y,*/}
-                    {/*left     : x,*/}
-                    {/*position : 'absolute',*/}
-                    {/*opacity  : this.state.animated.interpolate({*/}
-                    {/*inputRange : [0, 1],*/}
-                    {/*outputRange: [0.5, 1],*/}
-                    {/*}),*/}
-                    {/*transform: [{*/}
-                    {/*translateX: this.state.animated.interpolate({*/}
-                    {/*inputRange : [0, 1],*/}
-                    {/*outputRange: [0, w / 2 - (x + width / 2)],*/}
-                    {/*}),*/}
-                    {/*}, {*/}
-                    {/*translateY: this.state.animated.interpolate({*/}
-                    {/*inputRange : [0, 1],*/}
-                    {/*outputRange: [0, h / 2 - (y + height / 2)],*/}
-                    {/*}),*/}
-                    {/*}, {*/}
-                    {/*scaleX: this.state.animated.interpolate({*/}
-                    {/*inputRange : [0, 1],*/}
-                    {/*outputRange: [1, w / width],*/}
-                    {/*}),*/}
-                    {/*}, {*/}
-                    {/*scaleY: this.state.animated.interpolate({*/}
-                    {/*inputRange : [0, 1],*/}
-                    {/*outputRange: [1, w / width * (source[index].height / source[index].width)],*/}
-                    {/*}),*/}
-                    {/*},],*/}
-
-                    {/*// transform      : [{*/}
-                    {/*//     matrix: [*/}
-                    {/*//         w / width, 0, 0, 0,*/}
-                    {/*//         0, w / width * (source[index].height / source[index].width), 0, 0,*/}
-                    {/*//         0, 0, 1, 0,*/}
-                    {/*//         // 图片高度小于屏幕高度时*/}
-                    {/*//         // w / 2 - (x + width / 2), h / 2 - (y + height / 2), 0, 1,*/}
-                    {/*//         // 图片高度大于屏幕高度时*/}
-                    {/*//         w / 2 - (x + width / 2), (w / width * (source[index].height / source[index].width) * width) / 2 - (y + height / 2), 0, 1,*/}
-                    {/*//     ]*/}
-                    {/*// }],*/}
-                    {/*}}*/}
-                    {/*source={{*/}
-                    {/*cache     : 'force-cache',*/}
-                    {/*resizeMode: 'contain',*/}
-                    {/*uri       : source[index].uri,*/}
-                    {/*}}*/}
-                    {/*/>*/}
-                    {/*</TouchableWithoutFeedback>*/}
-                    {/*</ScrollView>*/}
-                    {/*</TouchableHighlight>*/}
+                                    <TouchableWithoutFeedback>
+                                        <Animated.Image
+                                            source={{
+                                                resizeMode: 'contain',
+                                                uri       : item.uri,
+                                                cache     : 'force-cache',
+                                            }}
+                                            style={{
+                                                width    : width,
+                                                height   : height,
+                                                top      : y,
+                                                left     : x,
+                                                position : 'absolute',
+                                                opacity  : this.state.animated.interpolate({
+                                                    inputRange : [0, 1],
+                                                    outputRange: [0.5, 1],
+                                                }),
+                                                transform: [
+                                                    {
+                                                        translateX: this.state.animated.interpolate({
+                                                            inputRange : [0, 1],
+                                                            outputRange: [0, w / 2 - (x + width / 2)],
+                                                        }),
+                                                    },
+                                                    {
+                                                        translateY: this.state.animated.interpolate({
+                                                            inputRange : [0, 1],
+                                                            outputRange: [0, scaleY * width > h ? (scaleY * width) / 2 - (y + height / 2) : h / 2 - (y + height / 2)],
+                                                        }),
+                                                    },
+                                                    {
+                                                        scaleX: this.state.animated.interpolate({
+                                                            inputRange : [0, 1],
+                                                            outputRange: [1, w / width],
+                                                        }),
+                                                    },
+                                                    {
+                                                        scaleY: this.state.animated.interpolate({
+                                                            inputRange : [0, 1],
+                                                            outputRange: [1, scaleY],
+                                                        }),
+                                                    },
+                                                ],
+                                            }}
+                                        />
+                                    </TouchableWithoutFeedback>
+                                </ScrollView>
+                            </TouchableHighlight>
+                        );
+                    })}
                 </Swiper>
             </Modal>
         );
