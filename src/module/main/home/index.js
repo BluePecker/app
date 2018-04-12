1b5136f525d964b87283552bf915bdd9e31ca25b
shuchao <645904877@qq.com>
2018-04-12 17:42:26 +0800

    ...

--- src/module/main/home/index.js
+++ src/module/main/home/index.js
@@ -8,7 +8,7 @@ import Detail from 'module/main/home/detail';
 import Snap from 'module/main/home/snap';
 
 export default StackNavigator({
-    'main/home/snap'  : {
+    'main/home/snap': {
         screen           : Snap,
         navigationOptions: ({navigation}) => ({
             headerTitle: (
@@ -38,42 +38,11 @@ export default StackNavigator({
             },
         }),
     },
-    'main/home/detail': {
-        screen           : Detail,
-        navigationOptions: ({navigation}) => ({header: <View/>,}),
-    },
 }, {
     transitionConfig : () => ({
         transitionSpec: {duration: 250},
     }),
     headerMode       : 'screen',
-    navigationOptions: {
-        //     headerTransparent: true,
-        //     headerTitle      : (
-        //         <Button styleName="clear">
-        //             <Text style={{
-        //                 fontSize  : 24,
-        //                 height    : 35,
-        //                 lineHeight: 45,
-        //                 color     : '#ffffff'
-        //             }}>微忆</Text>
-        //         </Button>
-        //     ),
-        //     headerLeft       : (
-        //         <Button styleName="clear">
-        //             <IonIcon size={26} name="md-menu" color="#ffffff"/>
-        //         </Button>
-        //     ),
-        //     headerRight      : (
-        //         <Button styleName="clear">
-        //             <IonIcon size={26} name="md-add" color="#ffffff"/>
-        //         </Button>
-        //     ),
-        //     headerStyle: {
-        //         backgroundColor  : '#FA729B',
-        //         borderBottomColor: 'transparent',
-        //         height           : 35,
-        //     },
-    },
+    navigationOptions: {},
     initialRouteName : 'main/home/snap',
 });
--- src/module/main/index.js
+++ src/module/main/index.js
@@ -1,83 +1,71 @@
 import React, {Component} from 'react';
-import {TabNavigator, TabBarBottom} from 'react-navigation';
+import {
+    StackNavigator,
+    TabNavigator,
+    TabBarBottom,
+} from 'react-navigation';
+import {View} from '@shoutem/ui';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import Home from 'module/main/home';
+import Detail from 'module/main/home/detail';
 import Mark from 'module/main/mark';
 import Notice from 'module/main/notice';
 import Mine from 'module/main/mine';
 
-export default TabNavigator({
-    "main/home"   : {
-        screen           : Home,
-        navigationOptions: {
-            tabBarLabel  : '首页',
-            tabBarIcon   : ({tintColor}) => <Icon name="ios-disc" size={24} color={tintColor}/>,
-        }
+export default StackNavigator({
+    'main'            : {
+        screen           : TabNavigator({
+            'main/home'   : {
+                screen           : Home,
+                navigationOptions: {
+                    tabBarLabel: '首页',
+                    tabBarIcon : ({tintColor}) => <Icon name="ios-disc" size={24} color={tintColor}/>,
+                }
+            },
+            'main/dig'    : {
+                screen           : Mark,
+                navigationOptions: {
+                    tabBarLabel: '发现',
+                    tabBarIcon : ({tintColor}) => <Icon name="md-wifi" size={24} color={tintColor}/>,
+                }
+            },
+            'main/message': {
+                screen           : Notice,
+                navigationOptions: {
+                    tabBarLabel: '消息',
+                    tabBarIcon : ({tintColor}) => <Icon name="md-notifications" size={24} color={tintColor}/>,
+                }
+            },
+            'main/mine'   : {
+                screen           : Mine,
+                navigationOptions: {
+                    tabBarLabel: '我的',
+                    tabBarIcon : ({tintColor}) => <Icon name="ios-contact" size={24} color={tintColor}/>,
+                }
+            },
+        }, {
+            tabBarComponent : TabBarBottom,
+            tabBarPosition  : 'bottom',
+            animationEnabled: false,
+            swipeEnabled    : false,
+            tabBarOptions   : {
+                inactiveTintColor: '#BDC3C7',
+                labelStyle       : {
+                    fontSize: 14,
+                },
+                activeTintColor  : '#FA729B',
+            },
+        }),
+        navigationOptions: ({navigation}) => ({header: <View/>,}),
     },
-    // "main/mark"   : {
-    //     screen           : Mark,
-    //     navigationOptions: {
-    //         tabBarLabel: '一瞬',
-    //         tabBarIcon : ({tintColor}) => <Icon name="md-finger-print" size={24} color={tintColor}/>,
-    //     }
-    // },
-    "main/dig"    : {
-        screen           : Mark,
-        navigationOptions: {
-            tabBarLabel: '发现',
-            tabBarIcon : ({tintColor}) => <Icon name="md-wifi" size={24} color={tintColor}/>,
-        }
-    },
-    "main/message": {
-        screen           : Notice,
-        navigationOptions: {
-            tabBarLabel: '消息',
-            tabBarIcon : ({tintColor}) => <Icon name="md-notifications" size={24} color={tintColor}/>,
-        }
-    },
-    "main/mine"   : {
-        screen           : Mine,
-        navigationOptions: {
-            tabBarLabel: '我的',
-            tabBarIcon : ({tintColor}) => <Icon name="ios-contact" size={24} color={tintColor}/>,
-        }
+    // 需要全屏展示的界面
+    'main/home/detail': {
+        screen           : Detail,
+        navigationOptions: ({navigation}) => ({header: <View/>,}),
     },
 }, {
-    navigationOptions: ({navigation}) => ({
-        tabBarIcon   : ({focused, tintColor}) => {
-            const {routeName} = navigation.state;
-            let iconName;
-            if (routeName === 'Home') {
-                iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-            } else if (routeName === 'Settings') {
-                iconName = `ios-options${focused ? '' : '-outline'}`;
-            }
-
-            // You can return any component that you like here! We usually use an
-            // icon component from react-native-vector-icons
-            return <Icon name={iconName} size={25} color={tintColor}/>;
-        },
-        // tabBarOnPress: event => {
-        //     console.log(event.scene.route.key);
-        //
-        //         console.log(event.scene.route.key === 'main/dig', event.scene.route.key);
-        //         event.scene.route.key === 'main/dig' && navigation.navigate('main/dig');
-        // },
-
-    }),
-    tabBarComponent  : TabBarBottom,
-    tabBarPosition   : 'bottom',
-    animationEnabled : false,
-    swipeEnabled     : false,
-    // animationEnabled : true,
-    // swipeEnabled     : true,
-    tabBarOptions    : {
-        //inactiveTintColor: 'gray',
-        inactiveTintColor: '#BDC3C7',
-        labelStyle       : {
-            fontSize: 14,
-        },
-        activeTintColor  : '#FA729B',
-    },
+    initialRouteName: 'main',
 });
+
+
