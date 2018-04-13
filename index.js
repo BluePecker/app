5ca0f232921558123a27e559aeec894d905ccaee
shuchao <645904877@qq.com>
2018-04-13 11:01:05 +0800

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import Application from './src/main';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 AppRegistry.registerComponent('app', () => Application);
