1cdac97c3decb0434efbc902e3601acad9c307f9
shuchao <645904877@qq.com>
2018-04-16 11:10:07 +0800

    ...

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import 'react-native-browser-polyfill';
 import {AppRegistry} from 'react-native';
 import Application from './src/main';
 
-// YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
 AppRegistry.registerComponent('app', () => Application);
